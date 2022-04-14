dbf59d718c54a66c9305335069348c068d8a335a
Slye <65019172+slybaidoo@users.noreply.github.com>
2022-04-14 10:34:24 +0000

    build: build sticky nav

--- js/script.js
+++ js/script.js
@@ -48,12 +48,35 @@ allLinks.forEach(function (link) {
 			sectionEl.scrollIntoView({ behavior: "smooth" });
 		}
 
-		// Close mobile naviagtion
+		// Close mobile navigation
 		if (link.classList.contains("main-nav-link")) headerEl.classList.toggle("nav-open");
 	});
 });
 ///////////////////////////////////////////////////////////
 // Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+	function (entries) {
+		const ent = entries[0];
+		console.log(ent);
+
+		if (ent.isIntersecting === false) {
+			document.body.classList.add("sticky");
+		}
+
+		if (ent.isIntersecting === true) {
+			document.body.classList.remove("sticky");
+		}
+	},
+	{
+		// In the viewport
+		root: null,
+		threshold: 0,
+		rootMargin: "-80px",
+	},
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
