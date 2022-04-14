b20bd0379026b4d1a75c6de3e9551eaec187b88a
Slye <65019172+slybaidoo@users.noreply.github.com>
2022-04-14 10:24:38 +0000

    buid: make mobile navigation work (toggle open and close class on click)

--- js/script.js
+++ js/script.js
@@ -19,6 +19,12 @@ yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+	headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
